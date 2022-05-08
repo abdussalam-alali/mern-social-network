1091bab209fd4504460091b3526044425b5da25c
Abdussalam Al-Ali <abdussalam.alali95@gmail.com>
2022-05-07 19:25:22 -0700

    Create login & register components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
 
-function Login(props) {
+    const { password, email }  = formData;
+    const updateState = e => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const handleSubmit = async e => {
+
+    }
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign into your Account</p>
+            <form className="form" onSubmit={(e) => handleSubmit(e)}>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" value={email} name="email"  onChange={e => updateState(e)} />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        name="password"
+                        onChange={e => updateState(e)}
+                        minLength="6"
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login"/>
+            </form>
+            <p className="my-1">
+               Don't have an account? <Link to="/register">Sign up</Link>
+            </p>
+        </>
     );
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    });
 
-function Register(props) {
+    const { name, password, password2, email }  = formData;
+    const updateState = e => setFormData({...formData, [e.target.name]: e.target.value});
+    const handleSubmit = async e => {
+        e.preventDefault();
+        if(password!==password2)
+            console.log('password doesnt match');
+        else
+        {
+            console.log("success");
+        }
+    }
     return (
-        <div>
-            Register
-        </div>
+        <>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={(e) => handleSubmit(e)}>
+                <div className="form-group">
+                    <input type="text" placeholder="Name" name='name' value={name} onChange={(e)=>{updateState(e)}} required/>
+                </div>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" value={email} name="email"  onChange={e => updateState(e)} />
+                    <small className="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+                        Gravatar email</small
+                    >
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        name="password"
+                        onChange={e => updateState(e)}
+                        minLength="6"
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        value={password2}
+                        onChange={e => updateState(e)}
+                        minLength="6"
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register"/>
+            </form>
+            <p className="my-1">
+                Already have an account? <a href="/login">Sign In</a>
+            </p>
+        </>
     );
 }
 
