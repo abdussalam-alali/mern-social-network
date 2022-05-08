fa3b45beb818e8f55589493697d32361c9da963f
Abdussalam Al-Ali <abdussalam.alali95@gmail.com>
2022-05-07 19:48:11 -0700

    init redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,22 @@ import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import  AppRoutes  from './routes/index';
 import Landing from "./components/layout/Landing";
-const App = ()=>
-    <Router>
-        <Fragment>
-            <Navbar />
-            <Routes>
-                <Route exact path='/' element={(<Landing />)} />
-            </Routes>
-            <section className='container'>
-                    <AppRoutes />
-            </section>
-        </Fragment>
-    </Router>
+import { Provider } from "react-redux";
+import store from './store';
 
+const App = ()=>
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Routes>
+                    <Route exact path='/' element={(<Landing />)} />
+                </Routes>
+                <section className='container'>
+                        <AppRoutes />
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
 
 export default  App;
